3a98b114917a2673d465e851d28249a40997b1bf
Premjeet-Singh <itsprem2193@gmail.com>
2016-08-19 17:51:47 -0700

    First commit

--- routes/index.js
+++ routes/index.js
@@ -23,20 +23,36 @@ router.get('/insert', function(req, res, next) {
 
 // Displayy All fields of dataset.......................
 router.get('/displayAll', function(req, res, next) {
-	var str = {};
+	var str = {},temp='';
 	db.serialize(function(){
+
+// var sql = "SELECT id,title,description,image_path,question,answer_one,answer_two,answer_three,answer_four,score_one,score_two,score_three,score_four,message_one,message_two,message_three,message_four,views,shares,view_total, shares_total from user";
+// 	db.all(sql,function(err,rows){
+// 		console.log(JSON.stringify(rows));
+// 	});
+
+	// console.log(req.body.answer);
 	
-		db.all("SELECT id,title,description,image_path,question,answer_one,answer_two,answer_three,answer_four,score_one,score_two,score_three,score_four,message_one,message_two,message_three,message_four,views,shares,view_total, shares_total from user", function(err, rows){
+		db.all("SELECT id,title,description,image_path,question,answer,score,message,views,shares,view_total, shares_total from user", function(err, rows){
 			rows.forEach(function (row) {
-			str[row.id] = [{
+				var temp_ans=row.answer;
+				var temp_score=row.score;
+				var temp_msg=row.message;
+				var ans_arr = temp_ans.split(",");
+				var score_arr = temp_score.split(",");
+				var message_arr = temp_msg.split(",");
+				console.log(ans_arr[0]);
+				console.log(temp_ans);
 				
+				str[row.id] = [{
+				id: row.id,
 				title: row.title, 
 				desc: row.description, 
 				path: row.image_path, 
 				ques: row.question, 
-				answer: [row.answer_one, row.answer_two, row.answer_three, row.answer_four],
-				score: [row.score_one, row.score_two, row.score_three, row.score_four],
-				messages: [row.message_one,row.message_two,row.message_three,row.message_four],
+				answer: ans_arr,
+				score: score_arr,
+				messages: message_arr,
 				Views: row.views,
 				Shares: row.shares,
 				Total_Views: row.view_total,
@@ -60,22 +76,28 @@ router.get('/displayOne/:id', function(req, res, next) {
 	var str = {};
 	db.serialize(function(){
 		// try {
-			db.each("SELECT id,title,description,image_path,question,answer_one,answer_two,answer_three,answer_four,score_one,score_two,score_three,score_four,message_one,message_two,message_three,message_four,views,shares,view_total, shares_total from user WHERE id = ?", req.params.id, function(err, row){
+			db.each("SELECT id,title,description,image_path,question,answer,score,message,views,shares,view_total, shares_total from user WHERE id = ?", req.params.id, function(err, row){
 				
 			// if(err){
 			// 	res.send("Id Not Match Error");
 			// 	throw err;
 			// }		// console.log('User id: '+row.id, row.title, row.description,row.image_path,row.question,row.answer_one,row.answer_two,row.answer_three,row.answer_four,row.score_one,row.score_two,row.score_three,row.score_four,row.views,row.shares,row.view_total,row.shares_total);
-			
+				var temp_ans=row.answer;
+				var temp_score=row.score;
+				var temp_msg=row.message;
+				var ans_arr = temp_ans.split(",");
+				var score_arr = temp_score.split(",");
+				var message_arr = temp_msg.split(",");
+				
 				str[row.id] = [{
 					 
 					title: row.title, 
 					desc: row.description, 
 					path: row.image_path, 
 					ques: row.question, 
-					answer: [row.answer_one, row.answer_two, row.answer_three, row.answer_four],
-					score: [row.score_one, row.score_two, row.score_three, row.score_four],
-					messages: [row.message_one,row.message_two,row.message_three,row.message_four],
+					answer: ans_arr,
+					score: score_arr,
+					messages: message_arr,
 					Views: row.views,
 					Shares: row.shares,
 					Total_Views: row.view_total,
@@ -146,17 +168,27 @@ router.post('/action', function(req, res, next) {
 
 	db.serialize(function(){
 		// Creating new USER Table....................
-		db.run("CREATE TABLE IF NOT EXISTS user (id INT, title VARCHAR, description VARCHAR, image_path VARCHAR, question VARCHAR, answer_one VARCHAR, answer_two VARCHAR, answer_three VARCHAR, answer_four VARCHAR, score_one INT, score_two INT, score_three INT, score_four INT, message_one VARCHAR, message_two VARCHAR, message_three VARCHAR, message_four VARCHAR, views INT, shares INT, view_total INT, shares_total INT)");
+		db.run("CREATE TABLE IF NOT EXISTS user (id INT, title VARCHAR, description VARCHAR, image_path VARCHAR, question VARCHAR, answer VARCHAR, score INT, message VARCHAR, views INT, shares INT, view_total INT, shares_total INT)");
 
+		var ans_arr1 = [req.body.ans1, req.body.ans2, req.body.ans3, req.body.ans4];
+		var ans_arr = ans_arr1.toString();
+		var msg = [req.body.message_one,req.body.message_two,req.body.message_three,req.body.message_four];
+		var msg_arr = msg.toString();
+		var score = [req.body.score1,req.body.score2,req.body.score3,req.body.score4];
+		var score_arr = score.toString();
+		// console.log(ans_arr);
 	// Inserting Form value to The USER Table.................
-		var stmt = db.prepare("INSERT into user values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)");
+		var stmt = db.prepare("INSERT into user values(?,?,?,?,?,?,?,?,?,?,?,?)");
 			
-			stmt.run(req.body.id, req.body.title, req.body.desc, req.body.imgpath, req.body.ques, req.body.ans1, req.body.ans2, req.body.ans3, req.body.ans4, req.body.score1, req.body.score2, req.body.score3, req.body.score4, req.body.message_one, req.body.message_two, req.body.message_three, req.body.message_four, req.body.views, req.body.shares, req.body.view_total, req.body.share_total); 
+		
+
+			stmt.run(req.body.id, req.body.title, req.body.desc, req.body.imgpath, req.body.ques, ans_arr, score_arr,msg_arr, req.body.views, req.body.shares, req.body.view_total, req.body.share_total); 
 		
 
 		stmt.finalize();
-		db.each("SELECT id,title,description,image_path,question,answer_one,answer_two,answer_three,answer_four,score_one,score_two,score_three,score_four,message_one,message_two,message_three,message_four,views,shares,view_total, shares_total from user", function(err, row){
-			console.log('User details: '+row.id, row.title, row.description,row.image_path,row.question,row.answer_one,row.answer_two,row.answer_three,row.answer_four,row.score_one,row.score_two,row.score_three,row.score_four,row.message_one,row.message_two,row.message_three,row.message_four,row.views,row.shares,row.view_total,row.shares_total);
+		db.each("SELECT id,title,description,image_path,question,answer,score,message,views,shares,view_total, shares_total from user", function(err, row){
+			console.log('User details: '+row.id, row.title, row.description,row.image_path,row.question,row.answer,row.score,row.message,row.views,row.shares,row.view_total,row.shares_total);
+			// res.json('User details: '+row.id, row.title, row.description,row.image_path,row.question,row.answer,row.score_one,row.score_two,row.score_three,row.score_four,row.message_one,row.message_two,row.message_three,row.message_four,row.views,row.shares,row.view_total,row.shares_total);
 		});
 	});
 
